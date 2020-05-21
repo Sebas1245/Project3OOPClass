87175427a923d5951a7bac829f4b57aa29a19b76
Sebastián Saldaña <A01570274@itesm.mx>
2020-05-21 14:22:10 -0500

    Initial declaration and implementation of Software class and added pragama once statement to Material class

---
+++ Software.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include "Material.h"
+
+using namespace std;
+
+class Software: public Material {
+    private: 
+        int  Version;
+        string SO;
+    public:
+        // constructors
+        Software();
+        Software(int, string);
+        // setters
+        void setVersion(int);
+        void setSO(string);
+        // getters
+        int getVersion();
+        string getSO();
+        // inherited abstract methods
+        void muestraDatos();
+        int cantidadDiasPrestamo();
+};
+
+// constructor implementation
+Software::Software(int v, string so) {
+    Version = v;
+    SO = so;
+}
+Software::Software() {
+    Version = -1;
+    SO = "N/D";
+}
+// setters implementation
+void Software::setVersion(int v) {
+    Version = v;
+}
+void Software::setSO(string so) {
+    SO = so;
+}
+// getters implementation
+int Software::getVersion() {
+    return Version;
+}
+string Software::getSO() {
+    return SO;
+}
+// implementation of inherited abstract methods
+void Software::muestraDatos() {
+    cout << "Tipo de material: Software \t" << "ID: " << idMaterial << " \t" << "Titulo: " << titulo << " \t";
+    cout << "Version: " << Version << " \t" << "Sistema operativo: " << SO << endl;  
+}
+
+int Software::cantidadDiasPrestamo() {
+    // Se presta solamente 1 dia predeterminadamente
+    return 1;
+}
