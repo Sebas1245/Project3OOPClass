340c91598dc6aea144bcbe9b30bb95362dc3256c
Sebastián Saldaña <A01570274@itesm.mx>
2020-05-21 20:34:24 -0500

    Initial declaration and implementation of Disco class

---
+++ Disco.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include "Material.h"
+
+using namespace std;
+
+class Disco : public Material {
+    private:
+        int duracion;
+        string Genero;
+    public:
+        // constructors
+        Disco(int, string);
+        Disco();
+        // setters
+        void setDuracion(int);
+        void setGenero(string);
+        // getters
+        int getDuracion();
+        string getGenero();
+        // inherited abstract methods
+        void muestraDatos();
+        int cantidadDiasPrestamo();
+};
+
+// constructors
+Disco::Disco(int num, string g) {
+    duracion = num;
+    Genero = g;
+}
+
+Disco::Disco() {
+    duracion = -1;
+    Genero = "N/D";
+}
+// implementation of setters
+void Disco::setDuracion(int num) {
+    duracion = num;
+}
+void Disco::setGenero(string g) {
+    Genero = g;
+}
+// implementation of getters
+int Disco::getDuracion() {
+    return duracion;
+}
+string Disco::getGenero() {
+    return Genero;
+}
+// implementation of inherited abstract methods
+void Disco::muestraDatos() {
+    cout << "Tipo de material: Libro \t" << "ID: " << idMaterial << " \t" << "Titulo: " << titulo << " \t";
+    cout << "Duracion: " << duracion << " \t" << "Genero: " << Genero << endl;
+}
+int Disco::cantidadDiasPrestamo() {
+    // se presta 7 dias predeterminadamente
+    return 2;
+}
