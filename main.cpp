6df6aa0c2796d18f9603d3163ebdf623941933da
Sebastián Saldaña <A01570274@itesm.mx>
2020-05-24 17:11:35 -0500

    Changed code for loading input file info to use dynamic memory and created code for options A and B on user menu

--- main.cpp
+++ main.cpp
@@ -55,16 +55,31 @@ int main(){
         int i = 0;
         while (archMaterial >> idM >> titulo >> claveM >> intVar >> strVar) {
             if(claveM == 'L') {
-                Libro libroNuevo(idM,titulo,intVar, strVar);
-                arrMaterialP[i] = &libroNuevo;
+                Libro *nuevoLibro;
+                nuevoLibro = new Libro();
+                nuevoLibro -> setIdMaterial(idM);
+                nuevoLibro -> setTitulo(titulo);
+                nuevoLibro -> setNumPag(intVar);
+                nuevoLibro -> setAutor(strVar);
+                arrMaterialP[i] = nuevoLibro;
             }
             else if(claveM == 'D') {
-                Disco discoNuevo(idM,titulo,intVar,strVar);
-                arrMaterialP[i] = &discoNuevo;
+                Disco *nuevoDisco;
+                nuevoDisco = new Disco();
+                nuevoDisco -> setIdMaterial(idM);
+                nuevoDisco -> setTitulo(titulo);
+                nuevoDisco -> setDuracion(intVar);
+                nuevoDisco -> setGenero(strVar);
+                arrMaterialP[i] = nuevoDisco;
             }
             else if(claveM == 'S') {
-                Software softwareNuevo(idM,titulo,intVar,strVar);
-                arrMaterialP[i] = &softwareNuevo;
+                Software *nuevoSoftware;
+                nuevoSoftware = new Software();
+                nuevoSoftware -> setIdMaterial(idM);
+                nuevoSoftware -> setTitulo(titulo);
+                nuevoSoftware -> setVersion(intVar);
+                nuevoSoftware -> setSO(strVar);
+                arrMaterialP[i] = nuevoSoftware;
             }
             else {
                 throw runtime_error("Error al cargar archivo de Materiales, clave no reconocida");
@@ -74,7 +89,7 @@ int main(){
         
     }
     catch(const runtime_error& e) {
-        cerr << e.what() << endl;
+        cout << e.what() << endl;
     }
     archMaterial.close();
     // end of trying to load info from Material file
@@ -90,7 +105,7 @@ int main(){
         arrReservas[j].setIdMaterial(idM);
         Fecha fechaReserva(dia,mes,anio);
         arrReservas[j].setFecha(fechaReserva);
-
+        j++;
     }
     archReservaciones.close();
     // end of loading Reservations info from input file
@@ -98,7 +113,59 @@ int main(){
     // run menu function to get the input option
     char respMenu;
     respMenu = menu();
+    do {
+        switch (respMenu) {
+            case 'A':
+                for(int i = 0; i < 30; i++) {
+                    if(arrMaterialP[i] -> getIdMaterial() != -1) {
+                        arrMaterialP[i] -> muestraDatos();
+                    }
+                }
+                break;
+            case 'B':
+                for(int i = 0; i < 30; i++) {
+                    if(arrReservas[i].getIdCliente() != -1 ) {
+                        Fecha fechaInicio = arrReservas[i].getFecha(), fechaFin;
+                        string tituloMaterial;
+                        // try to get the end date of Reserva and the name of the material
+                        try{
+                            for(int j = 0; j < 60; j++){
+                                if(arrReservas[i].getIdMaterial() == arrMaterialP[j] -> getIdMaterial()) {
+                                    fechaFin = arrReservas[i].calculaFechaFinReserva(arrMaterialP[j] -> cantidadDiasPrestamo());
+                                    tituloMaterial = arrMaterialP[j] -> getTitulo();
+                                }
+                                else {
+                                    throw runtime_error("Material no encontrado para la reserva del cliente" + arrReservas[i].getIdCliente());
+                                }
+                            }
+                        }
+                        catch(const runtime_error& e) {
+                            std::cerr << e.what() << '\n';
+                        }
+                        cout << "Reserva " << i << ") ";
+                        cout << "Inicio de reserva: " << fechaInicio;
+                        cout << "Fin de reserva: " << fechaFin;
+                        cout << "Titulo: " << tituloMaterial;
+                        cout << "ID Cliente: " << arrReservas[i].getIdCliente() << endl;
+                    }
+                }
+                break;
+            case 'C':
+                break;
+            case 'D':
+                break;
+            case 'E':
+                break;        
+            default:
+                cout << "Ha elegido terminar la sesión " << endl;
+                break;
+        }
+    } while (respMenu != 'F');
     
-       
+    // delete pointers to dynamically created objects during execution
+    for (int i = 0; i < 30; i++) {
+        delete arrMaterialP[i];
+    }
+    
     return 0;
 }
