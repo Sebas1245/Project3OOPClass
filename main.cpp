3e0bf444edc8b705b419e550dc351aa2fcf4fa6c
Sebastián Saldaña <A01570274@itesm.mx>
2020-05-25 18:35:13 -0500

    Added tracker of amount of objects in Materials pointer array, code for option C

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <assert.h>
 #include "Reserva.h"
 #include "Libro.h"
 #include "Software.h"
@@ -38,8 +39,9 @@ char menu(){
 }
 
 int main(){
-    // array of Material pointers
+    // array of Material pointers and global counter of objects in this array
     Material* arrMaterialP[30];
+    int objetosMaterial = 0;
     // array of Reservations
     Reserva arrReservas[60];
 
@@ -85,8 +87,8 @@ int main(){
                 throw runtime_error("Error al cargar archivo de Materiales, clave no reconocida");
             }
             i++;
+            objetosMaterial = i;
         }
-        
     }
     catch(const runtime_error& e) {
         cout << e.what() << endl;
@@ -113,24 +115,29 @@ int main(){
     do {
         // run menu function to get the input option
         respMenu = menu();
+        // declaration of variables that may be used in multiple cases
+        Fecha fechaInicio, fechaFin;
+        string tituloMaterial;
+        int idCliente;
         switch (respMenu) {
-            case 'A':
-                for(int i = 0; i < 30; i++) {
+            case 'A': {
+                for(int i = 0; i < objetosMaterial; i++) {
                     if(arrMaterialP[i] -> getIdMaterial() != -1) {
                         arrMaterialP[i] -> muestraDatos();
                     }
                 }
                 break;
-            case 'B':
+            }
+            case 'B': {
                 for(int i = 0; i < 60; i++) {
                     if(arrReservas[i].getIdCliente() != -1 ) {
-                        Fecha fechaInicio = arrReservas[i].getFecha(), fechaFin;
-                        int idCliente = arrReservas[i].getIdCliente();
-                        string tituloMaterial = "N/D";
+                        fechaInicio = arrReservas[i].getFecha(); 
+                        idCliente = arrReservas[i].getIdCliente();
+                        tituloMaterial = "N/D";
                         // try to get the end date of Reserva and the name of the material
                         try{
-                            for(int j = 0; j < 60; j++){
-                            if(arrReservas[i].getIdMaterial() == arrMaterialP[j] -> getIdMaterial()) {
+                            for(int j = 0; j < objetosMaterial; j++){
+                                if(arrReservas[i].getIdMaterial() == arrMaterialP[j] -> getIdMaterial()) {
                                     fechaFin = arrReservas[i].calculaFechaFinReserva(arrMaterialP[j] -> cantidadDiasPrestamo());
                                     tituloMaterial = arrMaterialP[j] -> getTitulo();
                                     break;
@@ -154,15 +161,66 @@ int main(){
                     }
                 }
                 break;
-            case 'C':
+            }    
+            case 'C': {
+                int inputIDMat, indexFoundID, intentos = 0;
+                bool idValido;
+                // ask and validate material ID
+                do {
+                    if(intentos > 0) {
+                        cout << "El ID introducido es invalido, intente de nuevo " << endl;
+                    }
+                    else {
+                        cout << "Introduzca el ID del material " << endl;
+                    }
+                    cin >> inputIDMat;
+                    for (int i = 0; i < objetosMaterial; i++){
+                        if(arrMaterialP[i] -> getIdMaterial() != inputIDMat) {
+                            idValido = false;
+                        }
+                        else {
+                            tituloMaterial = arrMaterialP[i] -> getTitulo();
+                            indexFoundID = i;
+                            idValido = true;
+                            break;
+                        }
+                    }
+                    intentos++;
+                }while (!idValido);
+                // display name of material 
+                cout << "Material: " << tituloMaterial << endl;
+                // get reservations with that specific material ID and show info if there is a match
+                int noOfReservation = 0;
+                for (int i = 0; i < 60; i++) {
+                    if(arrReservas[i].getIdMaterial() == inputIDMat) {
+                        noOfReservation++; 
+                        fechaInicio = arrReservas[i].getFecha();
+                        fechaFin = arrReservas[i].calculaFechaFinReserva(arrMaterialP[indexFoundID] -> cantidadDiasPrestamo());
+                        if(noOfReservation == 1) {
+                            cout << "Reservaciones actuales:" << endl;
+                        }
+                        cout << noOfReservation << ") ";
+                        cout << " Fecha inicio de reserva: " << fechaInicio << " \t";
+                        cout << " Final de reserva: " << fechaFin << endl;  
+                    }
+                    else if(noOfReservation == 0 && i == 59) {
+                        cout << "No se encontaron reservaciones para " << tituloMaterial << endl;
+                    }
+                }
                 break;
-            case 'D':
+            }
+            case 'D':{
+
                 break;
-            case 'E':
-                break;        
-            default:
+            }  
+            case 'E': {
+                break;
+            }
+                        
+            default:{
                 cout << "Ha elegido terminar la sesion " << endl;
                 break;
+            }
         }
     } while (respMenu != 'F');
     
